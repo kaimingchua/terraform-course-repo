41177d9cbe35299bf8a285b83363fde2d3b8e7a3
HashiCorp Training <training@hashicorp.com>
2022-07-05 02:14:25 +0000

    added new resource and output

--- primary/main.tf
+++ primary/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+resource "random_id" "random" {
+  keepers = {
+    uuid = uuid()
+  }
+
+  byte_length = 8
+}
+
 output "public_ip" {
   value = "8.8.8.8"
 }
@@ -17,4 +25,8 @@ output "destination" {
     "distance" = "1234567890"
     "date"     = "11 Jan 2018"
   }
+}
+
+output "random" {
+  value = random_id.random.hex
 }
